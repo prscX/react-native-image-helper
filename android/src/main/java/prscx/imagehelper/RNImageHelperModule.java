894fad20d670502760353f6ad534a1df62e3d683
Pranav Chauhan <pranav.chauhan@metricstream.com>
2020-05-25 10:15:59 +0530

    - adding support for url icons

--- android/src/main/java/prscx/imagehelper/RNImageHelperModule.java
+++ android/src/main/java/prscx/imagehelper/RNImageHelperModule.java
@@ -6,6 +6,10 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.Canvas;
 import android.graphics.Rect;
@@ -19,6 +23,12 @@ import com.facebook.react.bridge.ReadableMap;
 import android.graphics.drawable.Drawable;
 import android.annotation.TargetApi;
 import android.content.res.Resources;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class RNImageHelperModule extends ReactContextBaseJavaModule {
 
@@ -37,8 +47,27 @@ public class RNImageHelperModule extends ReactContextBaseJavaModule {
   @TargetApi(21)
   public static Drawable GenerateImage(ReadableMap props) {
     String name = props.getString("name");
+    String url = null;
 
-    if (name != null && name.length() > 0 && name.contains(".")) {
+    if (props.hasKey("url")) {
+      try {
+        ReadableMap iconUrl = props.getMap("url");
+        if (iconUrl.hasKey("uri")) {
+          url = iconUrl.getString("uri");
+        }
+      } catch (Exception e) { }
+    }
+
+    if (url != null && url.length() > 0) {
+      try {
+        Bitmap bitmap = drawableFromUrl(url);
+        return new BitmapDrawable(RNImageHelperModule.reactContext.getResources(), bitmap);
+      } catch(Exception e) {
+        Log.e("RNImageHelper", e.getMessage());
+      }
+
+      return null;
+    } else if (name != null && name.length() > 0 && name.contains(".")) {
       Resources resources = RNImageHelperModule.reactContext.getResources();
       name = name.substring(0, name.lastIndexOf("."));
 
@@ -47,6 +76,47 @@ public class RNImageHelperModule extends ReactContextBaseJavaModule {
     } else {
       return RNImageHelperModule.GenerateVectorIcon(props);
     }
+  }
+
+  private static Bitmap drawableFromUrl(String url) throws IOException {
+    Bitmap bitmap;
+    HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+    connection.setRequestProperty("User-agent","Mozilla/4.0");
+
+    connection.connect();
+    InputStream input = connection.getInputStream();
+
+    bitmap = BitmapFactory.decodeStream(input);
+
+    return getCircularBitmap(bitmap);
+  }
+
+
+  private static Bitmap getCircularBitmap(Bitmap srcBitmap) {
+    int squareBitmapWidth = Math.min(srcBitmap.getWidth(), srcBitmap.getHeight());
+
+    Bitmap dstBitmap = Bitmap.createBitmap(
+            squareBitmapWidth, // Width
+            squareBitmapWidth, // Height
+            Bitmap.Config.ARGB_8888 // Config
+    );
+
+    Canvas canvas = new Canvas(dstBitmap);
+
+    Paint paint = new Paint();
+    paint.setAntiAlias(true);
+
+    Rect rect = new Rect(0, 0, squareBitmapWidth, squareBitmapWidth);
+    RectF rectF = new RectF(rect);
+    canvas.drawOval(rectF, paint);
+    paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+    float left = (squareBitmapWidth-srcBitmap.getWidth())/2;
+    float top = (squareBitmapWidth-srcBitmap.getHeight())/2;
+    canvas.drawBitmap(srcBitmap, left, top, paint);
+
+    srcBitmap.recycle();
+
+    return dstBitmap;
   }
 
   @TargetApi(21)
